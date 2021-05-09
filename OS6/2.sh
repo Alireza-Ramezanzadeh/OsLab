fa2979d78819ab6c014a1e0392527f900add7dd6
Alireza Ramenzadeh <50357109+Alireza-Ramezanzadeh@users.noreply.github.com>
2021-05-10 01:21:21 +0430

    Add files via upload

---
+++ OS6/2.sh
@@ -0,0 +1,8 @@
+sudo apt-get install rar
+rar -p a filerar.rar file
+Alireza
+Alireza
+sudo apt install unrar
+Alireza
+unrar e filerar.rar ./
+Alireza
---
+++ OS6/3.sh
@@ -0,0 +1,7 @@
+sudo apt-get install zip unzip -y
+Alireza
+zip --encrypt filezip.zip file
+Alireza
+Alireza
+unzip filezip.zip
+Alirza
